nodejs/node

util: use private symbols in JS land directly
Instead of calling into C++ to use the private symbols, use an
ObjectTemplate to create an object that holds the symbols and
use them directly from JS land.

lib/internal/bootstrap/node.js
@@ -80,8 +80,9 @@ const {
   validateInteger,
 } = require('internal/validators');
 const {
-  exit_info_private_symbol,
-  getHiddenValue,
+  privateSymbols: {
+    exit_info_private_symbol,
+  },
   kExitCode,
   kExiting,
   kHasExitCode,
@@ -96,8 +97,7 @@ process.domain = null;
 
 // process._exiting and process.exitCode
 {
-  const fields = getHiddenValue(process, exit_info_private_symbol);
-
+  const fields = process[exit_info_private_symbol];
   ObjectDefineProperty(process, '_exiting', {
     __proto__: null,
     get() {

Njeru254/OpenAI

PR-URL: https://github.com/nodejs/node/pull/45379
Backport-PR-URL: https://github.com/nodejs/node/pull/45674
Reviewed-By: Chengzhong Wu <legendecas@gmail.com>
Reviewed-By: Antoine du Hamel <duhamelantoine1995@gmail.com>
Reviewed-By: James M Snell <jasnell@gmail.com>
Signed-off-by: Daeyeon Jeong <daeyeon.dev@gmail.com>

lib/internal/bootstrap/node.js
@@ -77,8 +77,9 @@ const {
   exposeInterface,
 } = require('internal/util');
 const {
-  exiting_aliased_Uint32Array,
-  getHiddenValue,
+  privateSymbols: {
+    exiting_aliased_Uint32Array,
+  },
 } = internalBinding('util');
 
 setupProcessObject();
@@ -88,8 +89,7 @@ setupBuffer();
 
 process.domain = null;
 {
-  const exitingAliasedUint32Array =
-    getHiddenValue(process, exiting_aliased_Uint32Array);
+  const exitingAliasedUint32Array = process[exiting_aliased_Uint32Array];
   ObjectDefineProperty(process, '_exiting', {
     __proto__: null,
     get() {